29ad2f5bd7d7264538e9f8ce17fb19d0ee662bc7
CHAN-YOON GO <31956098+Go-cy@users.noreply.github.com>
2022-01-18 01:56:03 +0900

    Update Solution.java

--- Programmers/코딩테스트 고득점 Kit/배열/H-Index/Solution.java
+++ Programmers/코딩테스트 고득점 Kit/배열/H-Index/Solution.java
@@ -2,22 +2,21 @@
 //새벽에 풀다자서 졸리고 생각안남 -> 사실 핑계고 ㅈㄴ멍청해서 못품 ^^ 자고 인나서 다시 풀어야함.
 //정확성 31.3% 
 //최대인용편수 h는 작성논문의 length을 넘을 수 없을 것 같은데,,졸린데,, 아 ! x랑 h[i]값이랑 같아야한다 ㅎㅎ 생각남
-// 하 9,11케이스 통과 실패,, 87.5% 자고 내일함.. 미비된 다른 조건 생각 필요.
+//하 9,11케이스 통과 실패,, 87.5% 자고 내일함.. 미비된 다른 조건 생각 필요.
+// 9빼고 다 통과,, ㅜㅜ 졸리다
 
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
-        int[] h = new int[citations.length];
         
         for (int i = 0; i < citations.length; i++)
         {
-            h[i] = i;
             int x = 0;
             int y = 0;
             
             for (int j = 0; j < citations.length; j++)
             {
-                if(citations[j] >= h[i])
+                if(citations[j] >= i)
                 {
                     x++;  
                 }
@@ -26,9 +25,9 @@ class Solution {
                     y++; 
                 }
             }
-            if(x >= y && h[i] == x)
+            if(x >= y && x >= i)
             {
-                answer = x;
+                answer = i;
             }
         }
         
