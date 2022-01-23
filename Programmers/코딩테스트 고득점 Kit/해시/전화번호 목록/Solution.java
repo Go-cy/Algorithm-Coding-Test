e2cc1e7bc3752154ef32b269350a639d2e53d62a
CHAN-YOON GO <31956098+Go-cy@users.noreply.github.com>
2022-01-23 23:37:33 +0900

    Update Solution.java

--- Programmers/코딩테스트 고득점 Kit/해시/전화번호 목록/Solution.java
+++ Programmers/코딩테스트 고득점 Kit/해시/전화번호 목록/Solution.java
@@ -1,23 +1,17 @@
 //배열로 풀면 효율성 3, 4 탈락임 해시로 다시풀어야함,, 자소서 작성때문에 내일 마저 풀어야함..
+// 찾다보니 String의 startsWith라는 함수가 있어서 이걸로 사용했더니 쉽게 풀림
+
+import java.util.Arrays;
 
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
+        Arrays.sort(phone_book);
         
-        for(int i = 0; i < phone_book.length; i++)
-        {
-            for( int j = 0; j < phone_book.length; j++)
-            {
-                if(i != j)
-                {
-                    if(phone_book[i].length() <= phone_book[j].length())
-                    {
-                        if(phone_book[i].equals(phone_book[j].substring(0,phone_book[i].length())))
-                            return false;
-                    }
-                }
-            }
-        }
+        for (int i = 0; i < phone_book.length - 1; i++) 
+            if (phone_book[i + 1].startsWith(phone_book[i])) 
+                return false;
+        
         return answer;
     }
 }
