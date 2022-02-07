0f80240980b6a72429fe73f37b0b7d836d284b1f
CHAN-YOON GO <31956098+Go-cy@users.noreply.github.com>
2022-02-07 13:22:13 +0900

    Create Solution.cs

---
+++ Programmers/코딩테스트 고득점 Kit/스택,큐/주식가격/Solution.cs
@@ -0,0 +1,21 @@
+//개 거지같은 지문은 제발 죽이자
+
+using System;
+
+public class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.Length];
+        
+        for(int i = 0; i < prices.Length; i ++)
+        {
+            for( int j = i+1; j < prices.Length; j++)
+            {
+                answer[i]++;
+                if(prices[i] > prices[j])
+                    break;
+            }
+        }
+        
+        return answer;
+    }
+}
