4b0df0dd5521ce7d2e130e73a085f059f4565178
CHAN-YOON GO <31956098+Go-cy@users.noreply.github.com>
2022-03-09 12:36:09 +0900

    Create 1.java

---
+++ BaekJoon/단계별/문자열/1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        char temp = sc.next().charAt(0);
+        int result = (int)temp;
+
+        System.out.println(result);
+    }
+}
